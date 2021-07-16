595dc75031d9a6a15510f3a59bcb70020cdc2620
Mostafa Omar <mostafa.omar.m.s@gmail.com>
2021-07-16 20:35:55 +0200

    Add error handlers for cancel reservation

--- src/Command/Presentation/Api/Middleware/ErrorHandler.ts
+++ src/Command/Presentation/Api/Middleware/ErrorHandler.ts
@@ -1,5 +1,7 @@
 import { ErrorRequestHandler } from "express";
 import ValidationError from "@app/Command/Application/Error/ValidationError";
+import ClientNotAuthorizedToCancelReservation from "@app/Command/Domain/Error/ClientNotAuthorizedToCancelReservation";
+import ReservationNotCancellable from "@app/Command/Domain/Error/ReservationNotCancellable";
 
 // TODO: so many possible throws
 const ErrorHandler: ErrorRequestHandler = (err, request, response, next) => {
@@ -7,7 +9,16 @@ const ErrorHandler: ErrorRequestHandler = (err, request, response, next) => {
     response.status(400).json({
       message: err.message,
     });
-  } else {
+  } else if (err instanceof ClientNotAuthorizedToCancelReservation) {
+    response.status(403).json({
+      message: "Client not authorized to cancel reservation",
+    });
+  } else if (err instanceof ReservationNotCancellable) {
+    response.status(409).json({
+      message: "Reservation not cancellable",
+    });
+  }
+  else {
     response.status(500).json({
       message: err.message,
     });
