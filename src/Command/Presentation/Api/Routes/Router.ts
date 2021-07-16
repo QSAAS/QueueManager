a3c35742c322b43bc1234131c7d6e88c16f8e352
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-07-16 05:17:34 +0200

    Remove useless QueueServerOperatorRouter

--- src/Command/Presentation/Api/Routes/Router.ts
+++ src/Command/Presentation/Api/Routes/Router.ts
@@ -3,7 +3,6 @@ import DependencyDefinitions, { DiEntry } from "@app/Command/Infrastructure/Conf
 import express from "express";
 import ErrorHandler from "@app/Command/Presentation/Api/Middleware/ErrorHandler";
 import createReservationRouter from "@app/Command/Presentation/Api/Routes/ReservationRouter";
-import createQueueServerOperatorRouter from "@app/Command/Presentation/Api/Routes/QueueServerOperatorRouter";
 import createQueueServerRouter from "@app/Command/Presentation/Api/Routes/QueueServerRouter";
 
 let container: DependencyInjectionContainer<DiEntry>;
@@ -20,7 +19,6 @@ async function createRouter(){
   const router = express.Router();
   const containerInstance = await getDependencyContainer();
   router.use("/reservation", createReservationRouter(containerInstance));
-  router.use("/operator", createQueueServerOperatorRouter(containerInstance));
   router.use("/server", createQueueServerRouter(containerInstance));
   router.use(ErrorHandler);
 
