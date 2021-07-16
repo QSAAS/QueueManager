c6e1bef6a997ce509c330204b9599c421cdc904a
Mostafa Omar <mostafa.omar.m.s@gmail.com>
2021-07-16 18:57:52 +0200

    Fix ReservationQueue returning InServiceReservations

--- src/Command/Infrastructure/Config/DependencyDefinitions.ts
+++ src/Command/Infrastructure/Config/DependencyDefinitions.ts
@@ -104,6 +104,7 @@ const definitions: DependencyDefinitions<DiEntry> = {
   [DiEntry.ReservationQueue]: (container) =>
     new FIFOReservationQueue(
       container.resolve(DiEntry.ActiveReservationRepository),
+      container.resolve(DiEntry.QueueServerOperatorRepository),
     ),
   [DiEntry.CancelReservationService]: (container) =>
     new CancelReservationService(
