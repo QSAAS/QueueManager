c1bc33c041913174221560b6c50a1860617fc5f2
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-07-16 05:21:32 +0200

    Add todos for error handler

--- src/Command/Presentation/Api/Middleware/ErrorHandler.ts
+++ src/Command/Presentation/Api/Middleware/ErrorHandler.ts
@@ -1,23 +1,17 @@
-import { ErrorRequestHandler } from "express";
+import {ErrorRequestHandler} from "express";
+import ValidationError from "@app/Command/Application/Error/ValidationError";
 
+// TODO: so many possible throws
 const ErrorHandler: ErrorRequestHandler = (err, request, response, next) => {
-  // if(err instanceof EmployeeNotAuthorizedError){
-  //   response.status(403).json({
-  //     message: "User is not authorized to carry this action"
-  //   });
-  // } else if (err instanceof ValidationError) {
-  //   response.status(400).json({
-  //     message: err.message,
-  //   });
-  // } else if (err instanceof IncorrectCredentialsError) {
-  //   response.status(401).json({
-  //     message: err.message,
-  //   })
-  // } else{
-  //   response.status(500).json({
-  //     message: err.message
-  //   });
-  // }
+  if (err instanceof ValidationError) {
+    response.status(400).json({
+      message: err.message,
+    });
+  } else {
+    response.status(500).json({
+      message: err.message,
+    })
+  }
   next(err);
 };
 
