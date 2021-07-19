3e2636af0bbf97a23ec3a22192ffaf22a75c92d7
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-07-19 06:06:55 +0200

    Add mark as free tests

--- tests/Presentation/Api/Routes/QueueServerRouter.test.ts
+++ tests/Presentation/Api/Routes/QueueServerRouter.test.ts
@@ -241,4 +241,163 @@ describe("/manager/server", () => {
         .expect(409);
     });
   });
+
+  describe("/mark-as-free", () => {
+    const PATH = "/manager/server/mark-as-free";
+
+    it("Should mark a busy queue server as free", async () => {
+      await queueServerOperatorRepository.getModel().create({
+        id: "::queueServerOperatorId::",
+        assignedQueueServerIds: ["::queueServerId::"],
+        assignedQueueNodeIds: [],
+        activeQueueServers: [{
+          id: "::queueServerId::",
+          reservation: {
+            id: "reservationId",
+            serviceStartTime: "1",
+          },
+        }],
+      });
+
+      await queueServerRepository.getModel().create({
+        id: "::queueServerId::",
+        assignedQueueNodeIds: [],
+      });
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+          queueServerId: "::queueServerId::",
+        })
+        .set("Accept", "application/json")
+        .expect(200);
+
+
+        const object = await queueServerOperatorRepository.getModel().findOne({id: "::queueServerOperatorId::"});
+        expect(object?.activeQueueServers.find(s => s.id === "::queueServerId::")?.reservation).toBeNull();
+    });
+
+    it ("Should return 400 when queueServerId is missing from request", async () => {
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+        })
+        .set("Accept", "application/json")
+        .expect(400);
+    });
+
+    it("Should return 400 when queueServerOperatorId is missing from request", async () => {
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+        })
+        .set("Accept", "application/json")
+        .expect(400);
+    });
+
+    it("Should return 404 when server is not found", async () => {
+      await queueServerOperatorRepository.getModel().create({
+        id: "::queueServerOperatorId::",
+        assignedQueueServerIds: ["::queueServerId::"],
+        assignedQueueNodeIds: [],
+        activeQueueServers: [],
+      });
+
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+          queueServerId: "::queueServerId::",
+        })
+        .set("Accept", "application/json")
+        .expect(404);
+    });
+
+    it("Should return 404 when operator is not found", async () => {
+      await queueServerRepository.getModel().create({
+        id: "::queueServerId::",
+        assignedQueueNodeIds: [],
+      });
+
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+          queueServerId: "::queueServerId::",
+        })
+        .set("Accept", "application/json")
+        .expect(404);
+    });
+
+    it("Should return 403 when operator is not allowed on server",async  () => {
+      await queueServerOperatorRepository.getModel().create({
+        id: "::queueServerOperatorId::",
+        assignedQueueServerIds: [],
+        assignedQueueNodeIds: [],
+        activeQueueServers: [],
+      });
+
+      await queueServerRepository.getModel().create({
+        id: "::queueServerId::",
+        assignedQueueNodeIds: [],
+      });
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+          queueServerId: "::queueServerId::",
+        })
+        .set("Accept", "application/json")
+        .expect(403);
+    });
+
+    it("Should return 409 when server is already free", async () => {
+      await queueServerOperatorRepository.getModel().create({
+        id: "::queueServerOperatorId::",
+        assignedQueueServerIds: ["::queueServerId::"],
+        assignedQueueNodeIds: [],
+        activeQueueServers: [{
+          id: "::queueServerId::",
+          reservation: null,
+        }],
+      });
+
+      await queueServerRepository.getModel().create({
+        id: "::queueServerId::",
+        assignedQueueNodeIds: [],
+      });
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+          queueServerId: "::queueServerId::",
+        })
+        .set("Accept", "application/json")
+        .expect(409);
+    });
+
+    it("Should return 409 when server is inactive", async () => {
+      await queueServerOperatorRepository.getModel().create({
+        id: "::queueServerOperatorId::",
+        assignedQueueServerIds: ["::queueServerId::"],
+        assignedQueueNodeIds: [],
+        activeQueueServers: [],
+      });
+
+      await queueServerRepository.getModel().create({
+        id: "::queueServerId::",
+        assignedQueueNodeIds: [],
+      });
+      await request(app)
+        .post(PATH)
+        .send({
+          queueServerOperatorId: "::queueServerOperatorId::",
+          queueServerId: "::queueServerId::",
+        })
+        .set("Accept", "application/json")
+        .expect(409);
+    });
+  });
 });
