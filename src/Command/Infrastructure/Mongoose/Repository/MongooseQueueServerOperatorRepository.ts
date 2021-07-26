3ccd572f6539caa7bd2e8927295a2af3e4f9e249
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-07-26 08:37:34 +0200

    Restore lean

--- src/Command/Infrastructure/Mongoose/Repository/MongooseQueueServerOperatorRepository.ts
+++ src/Command/Infrastructure/Mongoose/Repository/MongooseQueueServerOperatorRepository.ts
@@ -22,7 +22,7 @@ export default class MongooseQueueServerOperatorRepository implements QueueServe
   }
 
   public async getById(id: QueueServerOperatorId): Promise<QueueServerOperator> {
-    const object = await this.Model.findOne({ id: id.toString() });
+    const object = await this.Model.findOne({ id: id.toString() }).lean();
 
     if (!object) throw new QueueServerOperatorNotFound();
 
