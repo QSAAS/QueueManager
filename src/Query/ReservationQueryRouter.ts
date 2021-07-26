afce857d94797d72f16366d353c529f991386083
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-07-26 08:03:28 +0200

    Add expected waiting time query

--- src/Query/ReservationQueryRouter.ts
+++ src/Query/ReservationQueryRouter.ts
@@ -5,6 +5,8 @@ import {DiEntry} from "@app/Command/Infrastructure/Config/DependencyDefinitions"
 import DependencyInjectionContainer from "@app/Command/Infrastructure/Config/DependencyInjectionContainer";
 import QueueServerOperatorId from "@app/Command/Domain/ValueObject/QueueServerOperatorId";
 import IActiveReservation from "@app/Command/Infrastructure/Mongoose/Types/IActiveReservation";
+import MongooseQueueNodeStatsRepository
+  from "@app/Command/Infrastructure/Mongoose/Repository/MongooseQueueNodeStatsRepository";
 
 export default async function createReservationQueryRouter(container: DependencyInjectionContainer<DiEntry>) {
   const router = express.Router();
@@ -27,8 +29,23 @@ export default async function createReservationQueryRouter(container: Dependency
     try {
       const instance = await repo.getById(QueueServerOperatorId.from(id));
       const result = repo.getTransformer().mongooseObjectFrom(instance);
-      response.json(result);
+      const queueNodeStatsRepository = container
+        .resolve<MongooseQueueNodeStatsRepository>(DiEntry.QueueNodeStatsRepository)
+      let expectedWaitingTime: number|null = null;
+      try {
+        const queueNodeStats = await queueNodeStatsRepository.getByQueueNodeId(instance.getQueueNodeId());
+        expectedWaitingTime = queueNodeStats.getAverageServingTime();
+      } catch(e) {
+        console.log(e);
+      }
+      response.json({
+        ...result,
+        expectedWaitingTime: expectedWaitingTime === null ?
+          "No data about this queue node available"
+          : expectedWaitingTime
+      });
     } catch(e) {
+      console.log(e);
       response.status(404).json({
         message: `Reservation with id ${id} not found`,
       });
